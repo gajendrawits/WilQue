a5c5c83dd5c26579380a550cd3ad6841c527a1b0
Shardul Kumar <shardulkumar@WIL-Admins-Mac-mini-4.local>
2022-09-12 12:28:32 +0530

    feat: initial commit

---
+++ src/pages/homepage/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Logo from 'assets/svg/witslogo'
+import { useForm } from 'react-hook-form'
+import TextArea from 'components/FormElements/TextArea'
+import Button from 'components/Button'
+import { yupResolver } from '@hookform/resolvers/yup'
+import { MainContainer, Form, Error } from 'styles/pages/homepage'
+import { messageSchema } from 'utils/validations/homepage'
+import { FormData } from 'interface/pages/homepage'
+
+const HomePage = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(messageSchema),
+  })
+
+  const onSubmit = handleSubmit((data: any) => console.log(data))
+
+  return (
+    <MainContainer>
+      <Logo />
+      <Form onSubmit={onSubmit}>
+        <TextArea name="message" control={control} rows={4} placeholder="Drop your message" maxLength={100} />
+        <Error>{errors.message?.message}</Error>
+        <Button label="Post" />
+      </Form>
+    </MainContainer>
+  )
+}
+
+export default HomePage
