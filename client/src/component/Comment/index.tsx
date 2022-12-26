200393d4ae09d3f3a714f1291bd7c044c3cb9b55
jsshubham <shubham.joshi@thewitslab.com>
2022-12-26 17:24:26 +0530

    feat:Added fucntionality of comment

--- client/src/component/Comment/index.tsx
+++ client/src/component/Comment/index.tsx
@@ -3,14 +3,22 @@ import React, { useContext, useEffect, useState } from "react";
 import { QuestionContext } from "src/@core/context/QuestionContext";
 import usePost from "src/hooks/usePost";
 import router from "next/router";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import { Avatar } from "@material-ui/core";
 
 const index = (props: any) => {
+  const [profileDetails, setProfileDetails] = useState<any>();
+  useEffect(() => {
+    const a: any = localStorage.getItem("user");
+    setProfileDetails(JSON.parse(a));
+  }, []);
   const { getQuestionValue } = useContext(QuestionContext);
 
   const { question } = getQuestionValue;
   const [getCommentValue, setCommentValue] = useState("");
 
-  const [comment, setComment] = useState(false);
+  // const [comment, setComment] = useState(false);
 
   const { mutateAsync, isLoading, isSuccess, data } = usePost();
 
@@ -24,45 +32,38 @@ const index = (props: any) => {
       token: true,
     });
   };
+
   return (
     <>
-      <p
-        onClick={() => setComment(!comment)}
-        style={{
-          paddingLeft: "12px",
-          color: "blue",
-          cursor: "pointer",
-          width: "fit-content",
-        }}
-      >
-        Add a comment...
-      </p>
-      {comment ? (
-        <>
-          <TextareaAutosize
-            aria-label="minimum height"
-            minRows={1}
-            placeholder="Add a comment..."
-            style={{
-              width: "95%",
-              maxWidth: "95%",
-              marginLeft: "12px",
-              padding: "10px",
-              border: "none",
-            }}
-            onChange={(e: any) => setCommentValue(e.target.value)}
-          />
-          <Button
-            type="submit"
-            onClick={() => {
-              postComment();
-            }}
-            sx={{ width: "fit-content" }}
-          >
-            Post Comment
-          </Button>
-        </>
-      ) : null}
+      {props.showCommnetArea && (
+        <Stack direction="row" spacing={5}>
+          <>
+            <Avatar src={profileDetails?.profilePhoto} />
+            <TextareaAutosize
+              aria-label="minimum height"
+              minRows={1}
+              placeholder="Add a comment..."
+              style={{
+                width: "95%",
+                maxWidth: "80%",
+                marginLeft: "12px",
+                padding: "10px",
+              }}
+              onChange={(e: any) => setCommentValue(e.target.value)}
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              onClick={() => {
+                postComment();
+              }}
+              sx={{ width: "fit-content" }}
+            >
+              Comment
+            </Button>
+          </>
+        </Stack>
+      )}
     </>
   );
 };
