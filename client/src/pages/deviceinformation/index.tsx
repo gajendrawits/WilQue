a7651fb17841b0e92ae61218a9fac63160ccfbc4
kapooraman2 <100358497+kapooraman2@users.noreply.github.com>
2023-01-05 17:48:50 +0530

    feat/ui changes and screen size

--- client/src/pages/deviceinformation/index.tsx
+++ client/src/pages/deviceinformation/index.tsx
@@ -1,9 +1,31 @@
 import { Card, Typography } from "@mui/material";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Grid from "@mui/material/Grid";
+import Link from "@mui/material/Link";
 
 const index = () => {
   return (
     <>
+      <Grid item xs={12}>
+        <Typography
+          sx={{
+            width: "100%",
+            fontSize: "50px",
+          }}
+        >
+          <Link>Device information</Link>
+        </Typography>
+        <Typography
+          sx={{
+            width: "100%",
+            fontSize: "20px",
+            paddingBottom: "10px",
+          }}
+        >
+          Get information about your current device (screen size, pixel-ratio,
+          user agent, ...)
+        </Typography>
+      </Grid>
       <Card
         sx={{
           width: "100%",
@@ -27,63 +49,163 @@ const index = () => {
             className="card"
             sx={{
               display: "grid",
-              backgroundColor: "#2c2b2b34",
             }}
           >
-            <Typography 
-            sx={{
-              
-            }}
+            <Typography
+              sx={{
+                width: "100%",
+                fontSize: "25px",
+                alignItems: "center",
+                textAlign: "center",
+              }}
             >
-
+              Screen
+              <Grid
+                sx={{
+                  Width: "100%",
+                  paddingTop: "50px",
+                }}
+              >
+                {typeof window !== "undefined"
+                  ? screen.width + " " + screen?.height
+                  : null}
+              </Grid>
             </Typography>
-            Screen size
           </Card>
           <Card
             sx={{
               display: "grid",
-              backgroundColor: "#2c2b2b34",
             }}
           >
-            Orientation
+            <Typography
+              sx={{
+                width: "100%",
+                fontSize: "25px",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              Orientation
+              <Grid
+                sx={{
+                  Width: "100%",
+                  paddingTop: "50px",
+                }}
+              >
+                {typeof window !== "undefined"
+                  ? screen?.orientation.type
+                  : null}
+              </Grid>
+            </Typography>
           </Card>
           <Card
             sx={{
               display: "grid",
-              backgroundColor: "#2c2b2b34",
             }}
           >
-            gdjdfds
+            <Typography
+              sx={{
+                width: "100%",
+                fontSize: "25px",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              Orientation angle
+              <Grid
+                sx={{
+                  Width: "100%",
+                  paddingTop: "50px",
+                }}
+              >
+                {typeof window !== "undefined"
+                  ? screen?.orientation.angle
+                  : null}
+              </Grid>
+            </Typography>
           </Card>
           <Card
             sx={{
               display: "grid",
-              backgroundColor: "#2c2b2b34",
             }}
           >
-            gdjdfds
+            <Typography
+              sx={{
+                width: "100%",
+                fontSize: "25px",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              Color depth :
+              <Grid
+                sx={{
+                  Width: "100%",
+                  paddingTop: "50px",
+                }}
+              >
+                {typeof window !== "undefined" ? screen?.colorDepth : null}bits
+              </Grid>
+            </Typography>
           </Card>
           <Card
             sx={{
               display: "grid",
-              backgroundColor: "#2c2b2b34",
             }}
           >
-            gdjdfds
+            <Typography
+              sx={{
+                width: "100%",
+                fontSize: "25px",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              Pixel ratio
+              <Grid
+                sx={{
+                  Width: "100%",
+                  paddingTop: "50px",
+                }}
+              >
+                {typeof window !== "undefined"
+                  ? window?.devicePixelRatio
+                  : null}
+                dppx
+              </Grid>
+            </Typography>
           </Card>
           <Card
             sx={{
               display: "grid",
-              backgroundColor: "#2c2b2b34",
             }}
           >
-            gdjdfds
+            <Typography
+              sx={{
+                width: "100%",
+                fontSize: "25px",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              Window size :
+              <Grid
+                sx={{
+                  Width: "100%",
+                  paddingTop: "50px",
+                }}
+              >
+                {typeof window !== "undefined"
+                  ? screen.width + "  " + screen?.height
+                  : null}
+              </Grid>
+            </Typography>
           </Card>
         </Card>
         <Card
           sx={{
             width: "40%",
-            height: "600px",
+            height: "500px",
             display: "grid",
             backgroundColor: "#FFF",
             gridTemplateColumns: "repeat(auto-fill, minmax(15rem, 1fr))",
@@ -95,37 +217,107 @@ const index = () => {
             className="card"
             sx={{
               display: "grid",
-              backgroundColor: "#2c2b2b34",
             }}
           >
-            dghksghkdjhgkjdghkjshgkjhdsjkghjdshkjhkjdhg
+            <Typography
+              sx={{
+                width: "100%",
+                fontSize: "25px",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              Browser vendor :
+              <Grid
+                sx={{
+                  Width: "100%",
+                  paddingTop: "30px",
+                }}
+              >
+                {typeof window !== "undefined" ? window.navigator.vendor : null}
+              </Grid>
+            </Typography>
           </Card>
           <Card
             className="card"
             sx={{
               display: "grid",
-              backgroundColor: "#2c2b2b34",
             }}
           >
-            gdjdfdsdsdskjdhskjhdsjkhgkjdhskgjhkdh
+            <Typography
+              sx={{
+                width: "100%",
+                fontSize: "25px",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              Languages :
+              <Grid
+                sx={{
+                  Width: "100%",
+                  paddingTop: "30px",
+                }}
+              >
+                {typeof window !== "undefined"
+                  ? window.navigator.language
+                  : null}
+              </Grid>
+            </Typography>
           </Card>
           <Card
             className="card"
             sx={{
               display: "grid",
-              backgroundColor: "#2c2b2b34",
             }}
           >
-            gdjdfdsgndgdsg,dsjhdgkjshkjdglkhgdgh
+            <Typography
+              sx={{
+                width: "100%",
+                fontSize: "25px",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              Platform :
+              <Grid
+                sx={{
+                  Width: "100%",
+                  paddingTop: "30px",
+                }}
+              >
+                {typeof window !== "undefined"
+                  ? window.navigator.platform
+                  : null}
+              </Grid>
+            </Typography>
           </Card>
           <Card
             className="card"
             sx={{
               display: "grid",
-              backgroundColor: "#2c2b2b34",
             }}
           >
-            gdjdfds
+            <Typography
+              sx={{
+                width: "100%",
+                fontSize: "25px",
+                alignItems: "center",
+                textAlign: "center",
+              }}
+            >
+              User agent :
+              <Grid
+                sx={{
+                  Width: "100%",
+                  paddingTop: "30px",
+                }}
+              >
+                {typeof window !== "undefined"
+                  ? window.navigator.userAgent.slice(0, 9) + "..."
+                  : null}
+              </Grid>
+            </Typography>
           </Card>
         </Card>
       </Card>
